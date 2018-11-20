7fc84f5fa85e07824abd727dff7a3a1e65235559
Colton Potter <cpotter2@oberlin.edu>
2018-11-20 09:15:35 -0500

    Update Marker.js to use styled-components

--- src/Marker.js
+++ src/Marker.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 // import Wrapper from './MarkerStyle'
 
-const style = {
-width: '28px',
-heigth: '28px',
-borderRadius: '50%',
-color: 'yellow',
-padding: '14px',
-// fontWeight: 'bolder',
-// fontFamily: 'Arial',
-// fontSize: '10pt',
-// textAlign: 'center',
-border: '2pt solid blue',
-}
+const Button = styled.button`
+    width: 28px;
+    height: 28px;
+    border-radius: 50%;
+    color: yellow;
+    background-color: #ffff99;
+    padding: 14px;
+    //* / fontWeight: bolder;
+    // fontFamily: Arial;
+    // fontSize: 10pt;
+    // textAlign: center; */
+    border:2pt solid blue;
+`
 
 const Marker = props => {
     return (
-        <button style={style}></button>
+        <Button></Button>
         // <Wrapper/>
     )
 }
