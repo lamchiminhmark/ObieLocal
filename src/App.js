11e41d6ebf95a1fd35bea110c219a884ed5c3eb9
Colton Potter <cpotter2@oberlin.edu>
2018-11-20 09:24:27 -0500

    Update App.js with NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import MapContainer from './MapContainer';
+import Marker from './Marker';
+import NavBar from './NavBar';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MapContainer zoom={18}/>
+        <MapContainer zoom={18}>
+          {markers}
+        </MapContainer>
+        <NavBar />
       </div>
     );
   }
