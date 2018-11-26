fb95246be710fbabbfbfd73e6547bd504876b68e
Colton Potter <cpotter2@oberlin.edu>
2018-11-25 23:11:42 -0500

    Add queries route and DBHandler
    (queries.js) : See app.js to see that queries.js is mounted to the path '/query'; So, if the user types in {servername}/query, then express will use what is found in queries.js
    queries.js uses DBHandler to send all of the events in the Events table back to the front-end.
    (DBHandler.js) : This file contains some base functions for viewing the database. Another file just needs to 'require(DBHandler)' in order to use its functions. See queries.js

---
+++ server/routes/queries.js
@@ -0,0 +1,19 @@
+var database = require('../DBHandler');
+var express = require('express');
+var router = express.Router();
+
+// If the server is running and a GET request is sent
+// to this route, then the page will print out the titles
+// of all events in the Events table.
+router.get('/', function(req, res, next) {
+  database.tryConnection();
+  console.log('Received GET request!');
+  database
+    .selectAllEvents()
+    .then(response => Array.from(response))
+    .then(arr => arr.map(event => event.title))
+    .then(arr => res.send(arr))
+    .catch(err => res.send(err));
+});
+
+module.exports = router;
