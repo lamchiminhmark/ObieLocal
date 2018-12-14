d0a82f55607a0ef21f840b61a0f910195367325c
Thomas Nemeh <tnemeh@oberlin.edu>
2018-12-13 20:33:07 -0500

    post request handler code in request.js

--- server/routes/queries.js
+++ server/routes/queries.js
@@ -1,6 +1,14 @@
 var database = require('../DBHandler');
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser')
+router.use( bodyParser.json() );       // to support JSON-encoded bodies
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+router.use(express.json());       // to support JSON-encoded bodies
+router.use(express.urlencoded()); // to support URL-encoded bodies
 
 // If the server is running and a GET request is sent
 // to this route, then the page will print out the titles
@@ -16,6 +24,23 @@ router.get('/', function(req, res, next) {
       // console.log(arr);
       res.send(arr);})
     .catch(err => res.send(err));
+});
+
+/*
+fetch("http://localhost:3000/query", {
+    body: "email=test@example.com&password=pw",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: "post",
+})
+*/
+/*replace each quote with a backslash quote*/
+router.post('/', function(req, res, next) {
+  var body = req.body;
+  var contype = req.headers['content-type'];
+  console.log(body);
+  console.log(contype);
 });
 
 module.exports = router;
