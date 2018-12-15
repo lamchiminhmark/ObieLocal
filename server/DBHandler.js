2be044f62e5154569b5e56fd5beb5398c0f0c464
Colton Potter <cpotter2@oberlin.edu>
2018-12-15 11:02:57 -0500

    Update (DBHandler.js) INSERT
    1) Updated the INSERT script to accommodate for NULL values and numbers that have defaults
    2) Added an 'editorPool' to the script so that it has permission to insert into the database.
    3) Removed the catch() from the INSERT -- insertEvent() returns a Promise that should be caught by its caller

--- server/DBHandler.js
+++ server/DBHandler.js
@@ -1,15 +1,47 @@
 const mariadb = require('mariadb');
+
+// TODO - refactor so that validation happens inside of this
+// script, not inside of the queries.js route.
+
 // This bit sets the login and database for the 'mariadb' module
-const pool = mariadb.createPool({
+const viewerPool = mariadb.createPool({
   host: 'localhost',
   user: 'viewer',
   password: 'checkoutOBL',
   database: 'obielocal'
 });
 
+const editorPool = mariadb.createPool({
+  host: 'localhost',
+  user: 'editor',
+  password: 'changeThePocket',
+  database: 'obielocal'
+});
+
+const fieldList = [
+  'ID',
+  'title',
+  'created_at',
+  'updated_at',
+  'location_name',
+  'created_by',
+  'recurring',
+  'free',
+  'price',
+  'verified',
+  'venue_id',
+  'venue_url',
+  'filters',
+  'description',
+  'photo_url',
+  'address',
+  'latitude',
+  'longitude'
+];
+
 // Establishes the connection and prints out a success/failure message
 module.exports.tryConnection = function() {
-  pool
+  viewerPool
     .getConnection()
     .then(conn => {
       console.log('connected! connection id is ' + conn.threadId);
@@ -20,12 +52,11 @@ module.exports.tryConnection = function() {
     });
 };
 
-
-// ML: Returns a promise with a resolved 
+// ML: Returns a promise with a resolved
 // Returns all of the rows in the Users table
 module.exports.selectAllUsers = function() {
   return (
-    pool
+    viewerPool
       .query('SELECT * FROM Users')
       // .then(res => JSON.stringify(res))
       .catch(err => {
@@ -37,7 +68,7 @@ module.exports.selectAllUsers = function() {
 // Returns all of the rows in the Events table
 module.exports.selectAllEvents = function() {
   return (
-    pool
+    viewerPool
       .query('SELECT * FROM Events')
       // .then(rows => rows.map(item => item.username))
       // .then(res => JSON.stringify(res))
@@ -50,48 +81,64 @@ module.exports.selectAllEvents = function() {
 // Inserts the JSON object 'event' into the correct fields of the
 // database. Logs if there is an error. Does not do any validation.
 module.exports.insertEvent = function(event) {
-  return pool.query(
-    `INSERT INTO Events (
-      ID, 
-      title, 
-      created_at, 
-      updated_at, 
-      location_name, 
-      created_by, 
-      recurring, 
-      free, 
-      price, 
-      verified, 
-      venue_id, 
-      venue_url, 
-      filters, 
-      description, 
-      photo_url, 
-      address, 
-      latitude, 
-      longitude 
-    ) VALUES (
-      ${event.id},
-      '${event.title}',
-      '${event.created_at}',
-      '${event.updated_at}',
-      '${event.location_name}',
-      '${event.created_by}',
-      '${event.recurring}',
-      '${event.free}',
-      ${event.price},
-      '${event.verified}',
-      '${event.venue_id}',
-      '${event.venue_url}',
-      '${event.filters}',
-      '${event.description}',
-      '${event.photo_url}',
-      '${event.address}',
-      ${event.latitude},
-      ${event.longitude}
-    )` 
-  )
-  .catch( err => {
-    console.log(err);
-  });
+  /* Check for any undefined fields, and create them with 'NULL'
+  or 'DEFAULT if they are found. */
+  for (let i = 0; i < fieldList.length; i++) {
+    const field = fieldList[i];
+    if (event[field] === undefined) {
+      switch (field) {
+        case 'ID':
+        case 'recurring':
+        case 'free':
+        case 'verified':
+          event[field] = 'DEFAULT';
+          break;
+        default:
+          event[field] = 'NULL';
+      }
+    }
+  }
+
+  return editorPool
+    .query(
+      `INSERT INTO Events (
+        ID, 
+        title, 
+        created_at, 
+        updated_at, 
+        location_name, 
+        created_by, 
+        recurring, 
+        free, 
+        price, 
+        verified, 
+        venue_id, 
+        venue_url, 
+        filters, 
+        description, 
+        photo_url, 
+        address, 
+        latitude, 
+        longitude 
+      ) VALUES (
+        ${event.ID},
+        ${event.title},
+        ${event.created_at},
+        ${event.updated_at},
+        ${event.location_name},
+        ${event.created_by},
+        ${event.recurring},
+        ${event.free},
+        ${event.price},
+        ${event.verified},
+        ${event.venue_id},
+        ${event.venue_url},
+        ${event.filters},
+        ${event.description},
+        ${event.photo_url},
+        ${event.address} ,
+        ${event.latitude},
+        ${event.longitude}
+      )`
+    )
 };
