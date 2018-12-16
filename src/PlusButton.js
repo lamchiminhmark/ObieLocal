ce7f83dc9042200aeedbc7ef2d8c7364578ac5a0
Minh Lam <mlam@oberlin.edu>
2018-12-16 17:18:47 -0500

    feat(PlusButton): Removing unused fonts

--- src/PlusButton.js
+++ src/PlusButton.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 library.add(faUser);
 
