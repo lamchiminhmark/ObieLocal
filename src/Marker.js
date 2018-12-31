b90a18eeb8bf3e8594cab02cc276352f437e4dda
Colton Potter <cpotter2@oberlin.edu>
2018-12-31 13:26:22 -0500

    wip(Marker): Style improvements

--- src/Marker.js
+++ src/Marker.js
@@ -7,7 +7,8 @@ const Button = styled.button`
     border-radius: 50%;
     color: rgb(255, 184, 29);
     background-color: #cf102d;
-    /* background-color: ${props => props.backgroundColor || "rgb(255,255,255);"}; */
+    /* background-color: ${props =>
+      props.backgroundColor || 'rgb(255,255,255);'}; */
     opacity: ${props => props.opacity};
     padding: 14px;
     //* / fontWeight: bolder;
@@ -16,27 +17,33 @@ const Button = styled.button`
     // textAlign: center; */
     border:2pt solid #ffb81d;
     cursor: pointer;
-
-`
+`;
 
 class Marker extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.getOpacity = this.getOpacity.bind(this);
-    }
+    this.getOpacity = this.getOpacity.bind(this);
+  }
 
-    getOpacity() {
-        const hoursUntilStart = this.props.hoursUntilStart;
-        return (10 - hoursUntilStart) / 10;
-    }
+  getOpacity() {
+    const hoursUntilStart = this.props.hoursUntilStart;
+    return (10 - hoursUntilStart) / 10;
+  }
 
-    render() {
-
-        return (
-            <Button className={this.props.eventInfo.verified === 1 ? 'Marker-verified' : 'Marker-unverified'} onClick={() => this.props.handleMarkerClick(this.props.eventInfo)}opacity={this.getOpacity}></Button>
-        )    
-    }
+  render() {
+    return (
+      <Button
+        className={
+          this.props.eventInfo.verified === 1
+            ? 'Marker-verified'
+            : 'Marker-unverified'
+        }
+        onClick={() => this.props.handleMarkerClick(this.props.eventInfo)}
+        opacity={this.getOpacity}
+      />
+    );
+  }
 }
 
 export default Marker;
