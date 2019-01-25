5c31c80d741a327ad2b7b9b60622c9a8d31b2a72
Colton Potter <cpotter2@oberlin.edu>
2019-01-24 23:27:25 -0600

    feat(Marker): 'in progress' blinking animation
    For when an event is in progress

--- src/Marker.js
+++ src/Marker.js
@@ -9,9 +9,9 @@ const Button = styled.button`
   border-radius: 50%;
   color: rgb(255, 184, 29);
   animation-delay: ${props => props.animationDelay};
-  opacity: ${props => props.opacity};
+  /* opacity: ${props => props.opacity}; */
   border: 3px solid #ffb81d;
-
+  
   /* The rotating semicircle in the animation. */
   ::before {
     content: ' ';
@@ -24,7 +24,7 @@ const Button = styled.button`
     transform-origin: left;
     transform: rotate(0);
     overflow: hidden;
-
+    
     /* Each marker represents 6 hours, so the animation reflects that. */
     animation-name: spin, ${props => props.animationName};
     animation-duration: 10800s, 21600s;
@@ -33,6 +33,14 @@ const Button = styled.button`
     animation-play-state: paused;
     animation-delay: inherit;
   }
+`;
+
+const MarkerWrap = styled.div`
+  opacity: ${props => props.opacity};
+  animation-name: ${props => (props.blink ? 'marker-blink' : '')};
+  animation-duration: 1.5s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
 `;
 
 class Marker extends React.Component {
@@ -56,32 +64,37 @@ class Marker extends React.Component {
       opacity: 1.0,
       animationName: this.props.eventArray[0].verified
         ? 'bg-verified'
-        : 'bg-unverified'
+        : 'bg-unverified',
+      blink: 0
     };
     if (minutesUntilStart > 360) {
       displayData.animationDelay = '0s';
       displayData.opacity = 0.7;
     } else if (minutesUntilStart < 0) {
       displayData.animationDelay = '-21599s';
-      displayData.opacity = 0.1;
+      displayData.blink = 1;
     } else {
       const deg = 360 - minutesUntilStart;
       const sec = deg * 60;
       displayData.animationDelay = `-${sec}s`;
     }
     return displayData;
-  }
+  };
 
   render() {
     const verified = this.props.eventArray[0].verified;
     return (
-      <Button
-        className={verified ? 'Marker-verified' : 'Marker-unverified'}
-        onClick={() => this.props.handleMarkerClick(this.props.eventArray)}
+      <MarkerWrap
+        blink={this.displayData.blink}
         opacity={this.displayData.opacity}
-        animationName={this.displayData.animationName}
-        animationDelay={this.displayData.animationDelay}
-      />
+      >
+        <Button
+          className={verified ? 'Marker-verified' : 'Marker-unverified'}
+          onClick={() => this.props.handleMarkerClick(this.props.eventArray)}
+          animationName={this.displayData.animationName}
+          animationDelay={this.displayData.animationDelay}
+        />
+      </MarkerWrap>
     );
   }
 }
