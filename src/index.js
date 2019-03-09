f16f63826d68d599071097fd4e347c106663350c
Minh Lam <mlam@oberlin.edu>
2019-03-08 23:01:40 -0500

    feat: Set up basic firebaseand redux-debugging tools

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { rootReducer } from './store/reducers/RootReducer';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/reducers/RootReducer.js
@@ -0,0 +1,4 @@
+import {combineReducers} from 'redux';
+import {firebaseReducer} from 'react-redux-firebase';
+
+export const rootReducer = combineReducers({firebase: firebaseReducer});
