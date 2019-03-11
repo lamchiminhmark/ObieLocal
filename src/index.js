dd779b80d2316ff500faaa86fb48ca568529aac0
Minh Lam <mlam@oberlin.edu>
2019-03-10 21:03:38 -0400

    feat: Created reducers and actions for authentication

--- src/index.js
+++ src/index.js
@@ -4,22 +4,34 @@ import './index.css';
 import App from './App';
 import { rootReducer } from './store/reducers/RootReducer';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase })),
+    reactReduxFirebase(fbConfig, {
+      attachAuthIsReady: true
+    })
+  )
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: http://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+});
---
+++ src/store/actions/authActions.js
@@ -0,0 +1,50 @@
+export const signIn = credentials => {
+  return async (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    try {
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(credentials.email, credentials.password);
+      dispatch({ type: 'SIGN_IN_SUCCESS' });
+    } catch (err) {
+      dispatch({ type: 'SIGN_IN_ERROR', err });
+    }
+  };
+};
+
+export const signOut = () => {
+  return async (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    try {
+      await firebase.auth().signOut();
+      dispatch({ type: 'SIGN_OUT_SUCCESS' });
+    } catch (err) {
+      dispatch({ type: 'SIGN_OUT_ERROR' });
+    }
+  };
+};
+
+export const signUp = newUser => {
+  const { email, password, firstName, lastName } = newUser;
+  return async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    try {
+      const resp = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+      console.log(resp);
+      await firestore
+        .collection('users')
+        .doc(resp.user.uid)
+        .set({
+          firstName,
+          lastName,
+          initials: firstName[0] + lastName[0]
+        });
+      dispatch({ type: 'SIGN_UP_SUCCESS' });
+    } catch (err) {
+      dispatch({ type: 'SIGN_UP_ERROR', err: err.message });
+    }
+  };
+};
