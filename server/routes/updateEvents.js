3aa9053109ac61fba78c7c35dddffd8d1f5d4128
Colton Potter <cpotter2@oberlin.edu>
2019-03-31 16:53:58 -0400

    fix(updateEvents): Remove unnecessary try/catch

--- server/routes/updateEvents.js
+++ server/routes/updateEvents.js
@@ -3,30 +3,25 @@ var router = express.Router();
 var api = require('../APIQuery');
 
 router.get('/', (req, res, next) => {
-  try {
-    api
-      .updateDatabase()
-      .then(() => {
-        console.log('No errors in script.');
-        res.sendStatus(200);
-      })
-      .catch(reason => {
-        console.error('Update database / Promise Rejected: ' + reason);
-        if (reason.code && reason.code === 'PROTOCOL_CONNECTION_LOST') {
-          console.error('Database connection was closed.');
-        }
-        if (reason.code && reason.code === 'ER_CON_COUNT_ERROR') {
-          console.error('Database has too many connections.');
-        }
-        if (reason.code && reason.code === 'ECONNREFUSED') {
-          console.error('Database connection was refused.');
-        }
-        res.sendStatus(404);
-      });
-  } catch (err) {
-    console.error('Error found in updateEvents' + err);
-    res.sendStatus(500);
-  }
+  api
+    .updateDatabase()
+    .then(() => {
+      console.log('No errors in script.');
+      res.sendStatus(200);
+    })
+    .catch(reason => {
+      console.error('Update database / Promise Rejected: ' + reason);
+      if (reason.code && reason.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.');
+      }
+      if (reason.code && reason.code === 'ER_CON_COUNT_ERROR') {
+        console.error('Database has too many connections.');
+      }
+      if (reason.code && reason.code === 'ECONNREFUSED') {
+        console.error('Database connection was refused.');
+      }
+      res.sendStatus(404);
+    });
 });
 
 module.exports = router;
