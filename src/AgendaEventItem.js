048083a8dbbf29b000dc0c144eafd29c6a62882c
Colton Potter <cpotter2@oberlin.edu>
2019-04-01 07:52:10 -0400

    (AgendaEvent*): Style Improvements

--- src/AgendaEventItem.js
+++ src/AgendaEventItem.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./AgendaEventItem.css";
+import React from 'react';
+import './AgendaEventItem.css';
 import styled from 'styled-components';
 import ReactHtmlParser from 'react-html-parser';
 
@@ -12,7 +12,7 @@ const SeeMoreButton = styled.button`
   background-color: #cedd0e;
   color: #555;
   font-weight: bold;
-  
+
   :hover {
     background-color: #303030;
     color: #ededed;
@@ -21,18 +21,18 @@ const SeeMoreButton = styled.button`
 
 class AgendaEventItem extends React.Component {
   constructor(props) {
-      super(props);
-      this.timeFormatter = this.timeFormatter.bind(this);
+    super(props);
+    this.timeFormatter = this.timeFormatter.bind(this);
   }
 
-timeFormatter(start, end) {
-  let startTime = 'Time unknown.';
-  let endTime;
-  var startDate;
-  const dateTime = require('node-datetime');
+  timeFormatter(start, end) {
+    let startTime = 'Time unknown.';
+    let endTime;
+    var startDate;
+    const dateTime = require('node-datetime');
     // TODO: (CP) Do we actually need to check for start time in the event
     // object here?
-  if (start) {
+    if (start) {
       /* Note that the Date constructor automatically adjusts for timezone */
       const startTimeUTC = new Date(start);
       /*format times to display hour, minute, and period in 12 hour time*/
@@ -44,50 +44,63 @@ timeFormatter(start, end) {
       endTime = dateTime.create(endTimeUTC, 'I:M p').format();
     }
     endTime = endTime ? ` - ${endTime}` : ``;
-    
-    return `${startDate}, ${startTime}${endTime}`;
-}
 
-render() {
-    const { title, photo_url, location_name, start_time, end_time, description, verified} = this.props.event;
+    return `${startDate}, ${startTime}${endTime}`;
+  }
+
+  render() {
+    const {
+      title,
+      photo_url,
+      location_name,
+      start_time,
+      end_time,
+      description,
+      verified
+    } = this.props.event;
     var events = [];
     events.push(this.props.event);
     console.log(events);
     /*All roads specific function */
     const titleAllRoad = () => (
       <div className="event-title-allroad">
-          <h2>{title}</h2>
-          <img 
-            src="https://www.oberlin.edu/sites/default/files/styles/width_760/public/content/basic-page/header-image/all-roads-graphic-760.png?itok=oi9qcVb-" alt="All Roads Lead to Oberlin" />
-          
+        <h2>{title}</h2>
+        <img
+          src="https://www.oberlin.edu/sites/default/files/styles/width_760/public/content/basic-page/header-image/all-roads-graphic-760.png?itok=oi9qcVb-"
+          alt="All Roads Lead to Oberlin"
+        />
       </div>
-    )
+    );
     const titleNormal = () => (
       <div className="event-title">
-      <h2>{title}</h2>
-    </div>
-    )
+        <h2>{title}</h2>
+      </div>
+    );
 
-    const defaultImg = "https://d3e1o4bcbhmj8g.cloudfront.net/photos/204534/huge/ee2941c721f2cc491db9f4fb3abdf034ccad369c.jpg"
-    
+    const defaultImg =
+      'https://d3e1o4bcbhmj8g.cloudfront.net/photos/204534/huge/ee2941c721f2cc491db9f4fb3abdf034ccad369c.jpg';
+
     //If photo_url is null then set it to default image
     let photoURL;
     if (photo_url) {
-      photoURL = photo_url
-    }
-    else {
-      photoURL = defaultImg
+      photoURL = photo_url;
+    } else {
+      photoURL = defaultImg;
     }
 
     const whichTitle = () => {
-      if (!verified) {return titleAllRoad()} else {return titleNormal()}
-    }
+      if (!verified) {
+        return titleAllRoad();
+      } else {
+        return titleNormal();
+      }
+    };
 
     return (
       <li className="agenda-event-item">
         <div className="event-img-title">
           <img src={photoURL} alt={title} />
-         {whichTitle()}
+          {whichTitle()}
         </div>
         <div className="event-text">
           <div className="agenda-event-desc">
@@ -100,7 +113,7 @@ render() {
             </div>
             <div className="date_time">
               <span className="date_time_title">When? - </span>
-              <span>{this.timeFormatter(start_time,end_time)}</span>
+              <span>{this.timeFormatter(start_time, end_time)}</span>
             </div>
             <SeeMoreButton onClick={() => this.props.handleAgendaClick(events)}>
               See details >>
@@ -111,6 +124,5 @@ render() {
     );
   }
 }
-  
-  export default AgendaEventItem;
-  
+
+export default AgendaEventItem;
--- src/AgendaEventList.js
+++ src/AgendaEventList.js
@@ -1,57 +1,77 @@
-import React from "react";
-import AgendaEventItem from "./AgendaEventItem"
-
-import constants from './constants'
+import React from 'react';
+import AgendaEventItem from './AgendaEventItem';
 
 class AgendaEventList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            events: []
-        }
-        this.fetchData = this.fetchData.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
 
-    componentDidMount() {
-        this.fetchData();
-    }
+  componentDidMount() {
+    this.fetchData();
+  }
 
-    fetchData() {
-        fetch('https://obielocal-1541269219020.appspot.com/query')
-          // fetch("http://localhost:3001/query")
-          .then(response => response.json())
-          .then(data => {
-              this.storeEvents(data);
-              console.log(data);
-          }
-          )
-          .catch(error => console.error('Loading agenda failed ', error));
-      }
+  fetchData() {
+    fetch('https://obielocal-1541269219020.appspot.com/query')
+      // fetch("http://localhost:3001/query")
+      .then(response => response.json())
+      .then(data => {
+        this.storeEvents(data);
+        console.log(data);
+      })
+      .catch(error => console.error('Loading agenda failed ', error));
+  }
 
-      //Push fetched data into events, verified events come first.
-    storeEvents = data => {
-        const events = data.map(result => {
-          const  { id, title, photo_url, location_name, start_time, end_time, description, address, verified } = result;
-          return { id, title, photo_url, location_name, start_time, end_time, description, address, verified };
-        });
-        events.sort(function(a,b) {return a.verified-b.verified});
-        this.setState({ events });
-      }
-    
-        
-    render() {
-        return (
-        <section>
-            <ul className="AgendaEventList">
-            {
-                this.state.events.map( event => (
-                <AgendaEventItem key={event.id} event={event} handleAgendaClick={this.props.handleAgendaClick}/>
-                ))
-            }
-            </ul>
-        </section>
-        );
-    }   
+  //Push fetched data into events, verified events come first.
+  storeEvents = data => {
+    const events = data.map(result => {
+      const {
+        id,
+        title,
+        photo_url,
+        location_name,
+        start_time,
+        end_time,
+        description,
+        address,
+        verified
+      } = result;
+      return {
+        id,
+        title,
+        photo_url,
+        location_name,
+        start_time,
+        end_time,
+        description,
+        address,
+        verified
+      };
+    });
+    events.sort(function(a, b) {
+      return a.verified - b.verified;
+    });
+    this.setState({ events });
+  };
+
+  render() {
+    return (
+      <section>
+        <ul className="AgendaEventList">
+          {this.state.events.map(event => (
+            <AgendaEventItem
+              key={event.id}
+              event={event}
+              handleAgendaClick={this.props.handleAgendaClick}
+            />
+          ))}
+        </ul>
+      </section>
+    );
+  }
 }
 
 export default AgendaEventList;
