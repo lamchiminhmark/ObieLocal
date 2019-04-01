5b188a5f80f1dfea1b5fd18ccb6851e03e2e17ee
Colton Potter <cpotter2@oberlin.edu>
2019-04-01 08:00:49 -0400

    (Tabs): Style Improvements

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,68 +1,63 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Tab from './Tab';
-import './tabs.css'
+import './tabs.css';
 
 class Tabs extends Component {
-    static propTypes = {
-        children: PropTypes.instanceOf(Array).isRequired
-    }
+  static propTypes = {
+    children: PropTypes.instanceOf(Array).isRequired
+  };
 
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            activeTab: this.props.activeTab,
-        };
-    }   
+    this.state = {
+      activeTab: this.props.activeTab
+    };
+  }
 
-    onClickTabItem = (tab) => {
-        this.setState({ activeTab: tab });
-    }
+  onClickTabItem = tab => {
+    this.setState({ activeTab: tab });
+  };
 
-    componentWillReceiveProps() {
-        this.setState({activeTab: this.props.activeTab});
-    }
+  componentWillReceiveProps() {
+    this.setState({ activeTab: this.props.activeTab });
+  }
 
-    render() {
-        const {
-            onClickTabItem,
-            props: {
-                children
-            },
-            state: {
-                activeTab
-            }
-        } = this;
-        console.log("State in tabs");
-        console.log(this.state.activeTab);
-        return (
-            <div className="tabs">
-                <ol className="tab-list">
-                {children.map((child) => {
-                    const { label } = child.props;
+  render() {
+    const {
+      onClickTabItem,
+      props: { children },
+      state: { activeTab }
+    } = this;
+    console.log('State in tabs');
+    console.log(this.state.activeTab);
+    return (
+      <div className="tabs">
+        <ol className="tab-list">
+          {children.map(child => {
+            const { label } = child.props;
 
-                    return (
-                        <Tab
-                            activeTab={activeTab}
-                            key={label}
-                            label={label}
-                            onClick={onClickTabItem}
-                        />
-                    );
-                })
-                }
-                </ol>
-                <div className="tab-content">
-                {children.map((child) => {
-                    if (child.props.label !== activeTab) return undefined
-                    return child.props.children;
-                    })}
-                </div>
-            </div>
-        );
-    }
+            return (
+              <Tab
+                activeTab={activeTab}
+                key={label}
+                label={label}
+                onClick={onClickTabItem}
+              />
+            );
+          })}
+        </ol>
+        <div className="tab-content">
+          {children.map(child => {
+            if (child.props.label !== activeTab) return undefined;
+            return child.props.children;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Tabs;
