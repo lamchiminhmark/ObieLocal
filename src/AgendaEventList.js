c3fe9b36475ef45b86cdc4e78c1f959672dd471e
Colton Potter <cpotter2@oberlin.edu>
2019-04-04 07:43:31 -0400

    fix(AgendaEventList): Sort events by verified, then start_time

--- src/AgendaEventList.js
+++ src/AgendaEventList.js
@@ -3,8 +3,10 @@ import AgendaEventItem from './AgendaEventItem';
 
 const AgendaEventList = props => {
   // Sort events by not-verified first
-  const sortedEvents = [...props.events].sort(
-    (event1, event2) => event1.verified - event2.verified
+  const sortedEvents = [...props.events].sort((event1, event2) =>
+    event1.verified === event2.verified
+      ? new Date(event1.start_time) - new Date(event2.start_time)
+      : event1.verified - event2.verified
   );
 
   return (
