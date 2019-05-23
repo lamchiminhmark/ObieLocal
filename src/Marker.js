6aa943f3bb1f8c8d950511e29c27914c0e790bf2
Colton Potter <cpotter2@oberlin.edu>
2019-05-23 14:30:45 -0400

    fix(Marker): Fix property name

--- src/Marker.js
+++ src/Marker.js
@@ -114,7 +114,7 @@ class Marker extends React.Component {
           <div className="marker-text">
             <p className="numbers">
               {this.displayData.time}
-              <span style={{ 'font-size': '10px' }}>
+              <span style={{ fontSize: '10px' }}>
                 {this.displayData.amOrPm.substring(0, 1)}
               </span>
             </p>
