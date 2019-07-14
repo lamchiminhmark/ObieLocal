1a74047762a71161caa045e698596ab301413abf
Khang Nguyen <knguyend@oberlin.edu>
2019-07-14 14:05:30 +0700

    test: tests for recenterMap and make it sync func

--- src/actions/mapActions.js
+++ src/actions/mapActions.js
@@ -2,11 +2,10 @@ import { RECENTER_MAP } from './types';
 
 const SECRET_SAUCE_CONSTANT = 0.0001;
 
-export const recenterMap = ({ zoom, lat, lng }) => dispatch => {
-  dispatch({
+export const recenterMap = ({ zoom, lat, lng }) => {return {
     type: RECENTER_MAP,
     zoom: zoom || 17.5 + Math.random() * 0.01,
     lat: (lat || 41.2926) + (1 + Math.random()) * SECRET_SAUCE_CONSTANT,
     lng: (lng || -82.2183) - (1 + Math.random()) * SECRET_SAUCE_CONSTANT
-  });
-};
+  };
+}
