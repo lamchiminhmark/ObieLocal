6082b9bc040285659d27518bd6b3d39be113e6e1
Khang Nguyen <knguyend@oberlin.edu>
2019-07-24 23:29:02 +0700

    feat: actions and reducers for filter

--- src/components/MapContainer/index.js
+++ src/components/MapContainer/index.js
@@ -1,10 +1,10 @@
-import React, { Children } from 'react';
-import { connect } from 'react-redux';
-import GoogleMapReact from 'google-map-react';
-import mapStyle from './mapStyle';
-import styledMapCanvas from './styledMapCanvas.js';
-import config from '../../shared/config';
-import Marker from '../Marker'
+import React, { Children } from "react";
+import { connect } from "react-redux";
+import GoogleMapReact from "google-map-react";
+import mapStyle from "./mapStyle";
+import styledMapCanvas from "./styledMapCanvas.js";
+import config from "../../shared/config";
+import Marker from "../Marker";
 
 const toMarkerElement = markerObj => {
   return (
@@ -14,7 +14,24 @@ const toMarkerElement = markerObj => {
       eventArray={markerObj.events}
     />
   );
-}
+};
+
+const filterEventByDay = (marker, filterDay) => {
+  var newEvents = marker.events.filter(event => {
+    let date = new Date(event.start_time);
+    const realDate = date.setDate(date.getDate() - filterDay);
+    const now = new Date();
+    return now.getDate() === realDate;
+  });
+  marker.events = newEvents;
+  return marker;
+};
+
+const filterMarker = marker => {
+  if (!marker.props.lat && !marker.props.lng) return false;
+  if (marker.props.eventArray.length === 0) return false;
+  return true;
+};
 
 const MapContainer = props => {
   return (
@@ -22,7 +39,7 @@ const MapContainer = props => {
       <GoogleMapReact
         bootstrapURLKeys={{
           key: config.GOOGLE_MAP_API_KEY,
-          language: 'en'
+          language: "en"
         }}
         center={{ lat: props.lat, lng: props.lng }}
         zoom={props.zoom}
@@ -30,17 +47,21 @@ const MapContainer = props => {
       >
         {/*TECH_DEBT(KN): Clean this shit up */}
         {Children.toArray(
-          props.markers.map(obj => toMarkerElement(obj)).filter(marker => marker.props.lat || marker.props.lng)
+          props.markers
+          .map(marker => filterEventByDay(marker, props.filterDay)) //filter out events that are not in the specified date
+            .map(obj => toMarkerElement(obj))
+            .filter(filterMarker) // filter out markers that have empty eventArray or no lat/lng
         )}
       </GoogleMapReact>
     </div>
   );
 };
 
-const mapStateToProps = ({ events, map }) => {
+const mapStateToProps = ({ events, map, filter }) => {
   const { lat, lng, zoom } = map;
   const { allMarkers: markers } = events;
-  return { lat, lng, zoom, markers };
+  const { filterDay } = filter;
+  return { lat, lng, zoom, markers, filterDay };
 };
 
 export default connect(mapStateToProps)(MapContainer);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import eventsReducer from "./eventsReducer.js";
 import sidepaneReducer from "./sidepaneReducer.js";
 import mapReducer from './mapReducer.js';
+import filterReducer from  './filterReducer';
 
 export default combineReducers({
   events: eventsReducer,
   sidepane: sidepaneReducer,
   map: mapReducer,
+  filter: filterReducer
 });
