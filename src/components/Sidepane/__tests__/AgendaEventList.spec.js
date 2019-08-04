68ef766f26d0bd1bf739071817a044e67a665748
Khang Nguyen <knguyend@oberlin.edu>
2019-08-04 21:00:55 +0700

    test: AgendaEventList now tests multi-day

--- src/components/Sidepane/__tests__/AgendaEventList.spec.js
+++ src/components/Sidepane/__tests__/AgendaEventList.spec.js
@@ -20,33 +20,58 @@ function deepRender(state) {
 
   return { mockStore, wrapper };
 }
+
+const dt = new Date();
+const start1 = new Date("2019-06-05T05:00:00.000Z");
+start1.setDate(dt.getDate());
+const start2 = new Date("2019-06-05T05:30:00.000Z");
+start2.setDate(dt.getDate());
+const start3 = new Date("2019-06-05T05:15:00.000Z");
+start3.setDate(dt.getDate());
+const start4 = new Date("2019-06-05T05:30:00.000Z");
+start4.setDate(dt.getDate());
+const start5 = new Date("2019-06-05T05:20:00.000Z");
+start5.setDate(dt.getDate());
+const start6 = new Date("2019-06-05T05:20:00.000Z");
+start6.setDate(dt.getDate() + 7);
+const start7 = new Date();
+start7.setDate(dt.getDate() + 1);
+const state = {
+  filter: {
+    filterDay: 0
+  },
+  events: {
+    allMarkers: [
+      {
+        geo: { latitude: 5, longitude: 5 },
+        events: [
+          { ID: 1, verified: 1, start_time: start1 },
+          { ID: 2, verified: 1, start_time: start2 }
+        ]
+      },
+      {
+        geo: { latitude: 3, longitude: 3 },
+        events: [{ ID: 3, verified: 1, start_time: start3 }]
+      },
+      {
+        geo: { latitude: 1, longitude: 1 },
+        events: [
+          { ID: 4, verified: 0, start_time: start4 },
+          { ID: 5, verified: 0, start_time: start5 }
+        ]
+      },
+      {
+        geo: { latitude: 10, longitude: 10 },
+        events: [
+          { ID: 6, verified: 0, start_time: start6 },
+          { ID: 7, verified: 0, start_time: start7 }
+        ]
+      }
+    ]
+  }
+};
+
 describe("AgendaEventList Component", () => {
-  const state = {
-    events: {
-      allMarkers: [
-        {
-          geo: { latitude: 5, longitude: 5 },
-          events: [
-            { ID: 1, verified: 1, start_time: "2019-06-05T12:00:00.000Z" },
-            { ID: 2, verified: 1, start_time: "2019-06-05T12:30:00.000Z" }
-          ]
-        },
-        {
-          geo: { latitude: 3, longitude: 3 },
-          events: [
-            { ID: 3, verified: 1, start_time: "2019-06-05T12:15:00.000Z" }
-          ]
-        },
-        {
-          geo: { latitude: 1, longitude: 1 },
-          events: [
-            { ID: 4, verified: 0, start_time: "2019-06-05T12:20:00.000Z" },
-            { ID: 5, verified: 0, start_time: "2019-06-05T11:50:00.000Z" }
-          ]
-        }
-      ]
-    }
-  };
   const { wrapper, mockStore } = deepRender(state);
   it("Should render event in order (not-verified first, sooner start_time first)", () => {
     expect(wrapper.find(AgendaEventItem).get(0).key).toEqual("5");
@@ -64,24 +89,39 @@ describe("AgendaEventList Component", () => {
     let actions = mockStore.getActions();
     expect(actions.pop().type).toEqual("RECENTER_MAP");
     expect(actions.pop()).toEqual({
-        type: "SET_SELECTED_EVENTS",
-        selectedEventsArray: [
-          { ID: 5, lat: 1, lng: 1, verified: 0, start_time: "2019-06-05T11:50:00.000Z" }
-        ]
-      });
+      type: "SET_SELECTED_EVENTS",
+      selectedEventsArray: [
+        { ID: 5, lat: 1, lng: 1, verified: 0, start_time: start5 }
+      ]
+    });
     wrapper
       .find("button")
       .at(3)
       .simulate("click");
     actions = mockStore.getActions();
-    const recenter = actions.pop()
+    const recenter = actions.pop();
     expect(recenter.type).toEqual("RECENTER_MAP");
     expect(recenter.lat).toBeLessThan(4);
     expect(actions.pop()).toEqual({
-        type: "SET_SELECTED_EVENTS",
-        selectedEventsArray: [
-            { ID: 3, lat: 3, lng: 3, verified: 1, start_time: "2019-06-05T12:15:00.000Z" }
-          ]
-      }); 
+      type: "SET_SELECTED_EVENTS",
+      selectedEventsArray: [
+        { ID: 3, lat: 3, lng: 3, verified: 1, start_time: start3 }
+      ]
+    });
+  });
+});
+
+describe("AgendaEventList with multi-day filter", () => {
+  it("Should only render events happening tomorrow", () => {
+    const stateTomorrow = { ...state, filter: { filterDay: 1 } };
+    const { wrapper } = deepRender(stateTomorrow);
+    expect(wrapper.find(AgendaEventItem)).toHaveLength(1);
+    expect(wrapper.find(AgendaEventItem).get(0).key).toEqual("7");
+  });
+  it("Should only render events happening in 7 days", () => {
+    const stateTomorrow = { ...state, filter: { filterDay: 7 } };
+    const { wrapper } = deepRender(stateTomorrow);
+    expect(wrapper.find(AgendaEventItem)).toHaveLength(1);
+    expect(wrapper.find(AgendaEventItem).get(0).key).toEqual("6");
   });
 });
