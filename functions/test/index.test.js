826ce9b4564d5d2816f339a99eddd2d92cf83573
Minh Lam <mlam@oberlin.edu>
2020-05-09 01:45:02 +0200

    feat: Added test firebas project and passed a test

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,25 +1,38 @@
-const test = require('firebase-functions-test')();
+const test = require('firebase-functions-test')(
+  {
+    projectId: 'test-twine-12451'
+  },
+  './test-twine-12451-69359364facf.json'
+);
 const assert = require('chai').assert;
+// Importing functions
+const raccoonWrapper = require('../raccoon-wrapper');
 
 const USER_ID_1 = '10';
 const EVENT_ID_1 = '19';
 const EVENT_ID_2 = '20';
 
-// Importing functions
-const raccoonWrapper = require('../raccoon-wrapper');
-// Wrapping functions in a wrap method that
-async function asyncWrap() {
-  try {
-    await raccoonWrapper.rate(USER_ID_1, EVENT_ID_1, 'like');
-    await raccoonWrapper.rate(USER_ID_1, EVENT_ID_2, 'dislike');
-    assert.deepEqual(await raccoonWrapper.likedBy(EVENT_ID_1), [USER_ID_1]);
-    assert.deepEqual(await raccoonWrapper.dislikedBy(EVENT_ID_2), [USER_ID_1]);
-    await raccoonWrapper.rate(USER_ID_1, EVENT_ID_1, 'unlike');
-    await raccoonWrapper.rate(USER_ID_1, EVENT_ID_2, 'undislike');
-  } catch (e) {
-    console.log(e);
-  }
-  test.cleanup();
-}
+describe('rateEvent', () => {
+  it('should update the recommender database correctly with like and dislike', done => {
+    // Wrapping functions in a wrap method that
+    async function asyncWrap() {
+      try {
+        await raccoonWrapper.rate(USER_ID_1, EVENT_ID_1, 'like');
+        await raccoonWrapper.rate(USER_ID_1, EVENT_ID_2, 'dislike');
+        assert.deepEqual(await raccoonWrapper.likedBy(EVENT_ID_1), [USER_ID_1]);
+        assert.deepEqual(await raccoonWrapper.dislikedBy(EVENT_ID_2), [
+          USER_ID_1
+        ]);
+        await raccoonWrapper.rate(USER_ID_1, EVENT_ID_1, 'unlike');
+        await raccoonWrapper.rate(USER_ID_1, EVENT_ID_2, 'undislike');
+      } catch (e) {
+        console.log(e);
+      }
+      test.cleanup();
+    }
 
-return asyncWrap();
+    asyncWrap()
+      .then(() => done())
+      .catch(e => assert.fail(e));
+  });
+});
