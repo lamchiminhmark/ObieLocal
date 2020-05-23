76dbbf30420da46ee8eb9539c7b1f94ceaa32f2a
Colton Potter <cpotter2@oberlin.edu>
2020-05-23 18:08:12 -0400

    wip: modernize store

--- src/store.js
+++ src/store.js
@@ -1,20 +1,17 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
-import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
-import fbConfig from "./shared/fbConfig";
 
 const initialState = {};
+
+const middleware = [thunk]
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
   composeEnhancers(
-    applyMiddleware(thunk.withExtraArgument({ getFirebase })),
-    reactReduxFirebase(fbConfig, {
-      attachAuthIsReady: true
-    })
+    applyMiddleware(...middleware)
   )
 );
 
