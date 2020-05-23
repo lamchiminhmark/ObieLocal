cbcdd0ca27e0f04d25dd1f7d1cd523ee55027333
Minh Lam <mlam@oberlin.edu>
2020-05-23 14:07:20 +0700

    wip: Could fetch generic behaviour rec

--- src/components/Recommender/index.js
+++ src/components/Recommender/index.js
@@ -13,16 +13,18 @@ import { firestoreConnect } from 'react-redux-firebase';
 
 // TODO(ML): This might be a use case for render nested child
 const Recommender = props => {
-    console.log(props.behaviorRec)
-    return (<div>{props.behaviorRec}</div>)
+    // console.log(props.behaviorRec)
+    return (<div>hi</div>)
 }
 
-const mapStateToProps = ({ firestore: { data } }) => {
-    const uid = '1FX9PWN8H4TreVGKEwoxxrXLWCc2';
-    return { behaviorRec: data.users && data.users[uid] }
+const mapStateToProps = ({ firebase: { auth }, firestore: { data } }) => {
+    return { auth, behaviorRec: data.users && data.users[auth.uid] }
 }
 
 export default compose(
-    firestoreConnect(() => [{ collection: 'users', doc: '1FX9PWN8H4TreVGKEwoxxrXLWCc2' }]),
-    connect(mapStateToProps)
+    connect(mapStateToProps),
+    firestoreConnect((props) => {
+        // console.log(props.auth.uid);
+        return [{ collection: 'users', doc: props.auth.uid }]
+    })
 )(Recommender);
