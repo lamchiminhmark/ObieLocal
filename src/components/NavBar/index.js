3517fbee0224b8abc90a00e775e47721754b030a
Colton Potter <cpotter2@oberlin.edu>
2020-05-24 15:53:43 -0400

    wip: Convert NavBar to functional / LogOut works

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,108 +1,97 @@
 /* Container */
 
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { toggleSidepane } from "../../actions/sidepaneActions";
-import Popup from "./Popup";
-import MenuButton from "./MenuButton";
-import { StyledNavContainer, StyledNav } from "./styles";
-import {signOut} from '../../actions/authActions';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { toggleSidepane } from '../../actions/sidepaneActions';
+import Popup from './Popup';
+import MenuButton from './MenuButton';
+import { StyledNavContainer, StyledNav } from './styles';
+import { useFirebase } from 'react-redux-firebase';
 
-class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: "none"
-    };
+const NavBar = (props) => {
+  const [show, setShow] = useState('none');
 
-    this.togglePopup = this.togglePopup.bind(this);
+  const firebase = useFirebase();
+
+  const signOut = () => {
+    firebase.auth().signOut().then(console.log('Signed out!'))
   }
 
-  togglePopup(e) {
-    const name = e.target.getAttribute("id");
+  const togglePopup = (e) => {
+    const name = e.target.getAttribute('id');
     switch (name) {
-      case "aboutBtn":
-        this.setState({ show: "about" });
+      case 'aboutBtn':
+        setShow('about');
         break;
-      case "contactBtn":
-        this.setState({ show: "contact" });
+      case 'contactBtn':
+        setShow('contact');
         break;
-      case "useBtn":
-        this.setState({ show: "use" });
+      case 'useBtn':
+        setShow('use');
         break;
-      case "signUp":
-        this.setState({ show: "signUp" });
+      case 'signUp':
+        setShow('signUp');
         break;
       default:
-        this.setState({ show: "none" });
+        setShow('none');
     }
+  };
+
+  let popupElement = null;
+  if (show !== 'none') {
+    popupElement = <Popup type={show} handleClose={togglePopup} />;
   }
-
-  render() {
-    let popupElement = null;
-    if (this.state.show !== "none") {
-      popupElement = (
-        <Popup type={this.state.show} handleClose={this.togglePopup} />
-      );
-    }
-    const { loggedIn } = this.props;
-    let authButton;
-    if (loggedIn) {
-      authButton = (
-        <button id="logOut" onClick={this.props.signOut}>
-          Log Out
-        </button>
-      );
-    } else {
-      authButton = (
-        <button id="signUp" onClick={this.togglePopup}>
-          Sign Up
-        </button>
-      );
-    }
-
-    return (
-      <StyledNavContainer id="interface">
-        <StyledNav id="navbar">
-          <MenuButton handleMenuClick={this.props.toggleSidepane} />
-          <h2>ObieLocal</h2>
-          <ul>
-            <li key="1">
-              <button id="aboutBtn" onClick={this.togglePopup}>
-                About
-              </button>
-            </li>
-            <li key="2">
-              <button id="contactBtn" onClick={this.togglePopup}>
-                Contact
-              </button>
-            </li>
-            <li key="3">
-              {authButton}
-            </li>
-          </ul>
-          {popupElement}
-        </StyledNav>
-      </StyledNavContainer>
+  const { loggedIn } = props;
+  let authButton;
+  if (loggedIn) {
+    authButton = (
+      <button id="logOut" onClick={signOut}>
+        Log Out
+      </button>
+    );
+  } else {
+    authButton = (
+      <button id="signUp" onClick={togglePopup}>
+        Sign Up
+      </button>
     );
   }
-}
 
-const mapDispatchToProps = dispatch => {
+  return (
+    <StyledNavContainer id="interface">
+      <StyledNav id="navbar">
+        <MenuButton handleMenuClick={props.toggleSidepane} />
+        <h2>ObieLocal</h2>
+        <ul>
+          <li key="1">
+            <button id="aboutBtn" onClick={togglePopup}>
+              About
+            </button>
+          </li>
+          <li key="2">
+            <button id="contactBtn" onClick={togglePopup}>
+              Contact
+            </button>
+          </li>
+          <li key="3">{authButton}</li>
+        </ul>
+        {popupElement}
+      </StyledNav>
+    </StyledNavContainer>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
   return {
     toggleSidepane: () => dispatch(toggleSidepane()),
-    signOut: () => dispatch(signOut())
   };
 };
 
 const mapStateToProps = ({ firebase, auth }) => {
   return {
     loggedIn: firebase.auth.uid ? true : false,
-    err: auth.err || ""
+    err: auth.err || '',
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(NavBar);
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
