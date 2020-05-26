6bdc630e8b04bf8232631a6d2b37a5eb161f2361
Minh Lam <mlam@oberlin.edu>
2020-05-26 18:24:54 +0700

    wip: Social recommendation generated

--- src/components/Recommender/index.js
+++ src/components/Recommender/index.js
@@ -15,11 +15,15 @@ import { firestoreConnect } from 'react-redux-firebase';
 const Recommender = props => {
     // console.log(props.behaviorRec)
 
-    // Generate behaviour rec 
-    const behaviorRec = props.user && props.user.events.recommended;
 
-    // Generate social recommendations
-    // const socialRec = getSocialRec(props.followees, followeeProfiles);
+    if (props.users) {
+        // Generate behaviour rec 
+        const behaviorRec = props.user && props.user.events.recommended;
+
+        // Generate social recommendations
+        const socialRec = getSocialRec(props.followees, props.users);
+        console.log(socialRec);
+    }
 
     // I need to do array-contains-any query through the events database until 
 
@@ -31,16 +35,25 @@ const Recommender = props => {
 
 /**
  * Returns an Object of {event: n} where each event have n interested followees
+ * Note: If the followee's records cannot be found, she does not impact the recommendation 
  * @param {uid[]} followees 
  * @param {User[]} users 
  */
 const getSocialRec = (followees, users) => {
     const toReturn = {};
     followees.forEach(followee => {
+        if (!users[followee]) return;
         users[followee]
             .events
             .interested
-            .forEach(event => toReturn[event] ? toReturn[event]++ : toReturn[event] = 1)
+            .map(event => event.trim())                 // Remove white spaces on the edges
+            .forEach(event => toReturn[event] ? toReturn[event]++ : toReturn[event] = 1);
+
+        users[followee]
+            .events
+            .going
+            .map(event => event.trim())                 // Remove white spaces on the edges
+            .forEach(event => toReturn[event] ? toReturn[event]++ : toReturn[event] = 1);
     });
     return toReturn;
 }
@@ -50,6 +63,7 @@ const mapStateToProps = ({ firebase: { auth }, firestore: { data } }) => {
         auth,
         user: data.users && data.users[auth.uid],
         followees: data.users && data.users[auth.uid].follow.followees,
+        users: data.users,
         events: data.events
     }
 }
