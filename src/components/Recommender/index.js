03dde02633bf09d7a8e083580dfb537fa8d08a36
Minh Lam <mlam@oberlin.edu>
2020-05-26 17:01:22 +0700

    wip: Other people's profile accessible thru firestore

--- src/components/Recommender/index.js
+++ src/components/Recommender/index.js
@@ -14,17 +14,60 @@ import { firestoreConnect } from 'react-redux-firebase';
 // TODO(ML): This might be a use case for render nested child
 const Recommender = props => {
     // console.log(props.behaviorRec)
+
+    // Generate behaviour rec 
+    const behaviorRec = props.user && props.user.events.recommended;
+
+    // Generate social recommendations
+    // const socialRec = getSocialRec(props.followees, followeeProfiles);
+
+    // I need to do array-contains-any query through the events database until 
+
+    // Generate interest recommendations
+
+    // Combine the recommendations
     return (<div>hi</div>)
 }
 
+/**
+ * Returns an Object of {event: n} where each event have n interested followees
+ * @param {uid[]} followees 
+ * @param {User[]} users 
+ */
+const getSocialRec = (followees, users) => {
+    const toReturn = {};
+    followees.forEach(followee => {
+        users[followee]
+            .events
+            .interested
+            .forEach(event => toReturn[event] ? toReturn[event]++ : toReturn[event] = 1)
+    });
+    return toReturn;
+}
+
 const mapStateToProps = ({ firebase: { auth }, firestore: { data } }) => {
-    return { auth, behaviorRec: data.users && data.users[auth.uid] }
+    return {
+        auth,
+        user: data.users && data.users[auth.uid],
+        followees: data.users && data.users[auth.uid].follow.followees,
+        events: data.events
+    }
+}
+
+const firestoreConnectArgs = props => {
+    const followeesConnection = props.followees ?
+        props.followees.map(followee =>
+            ({ collection: 'users', doc: followee })
+        ) :
+        [];
+    return [
+        { collection: 'users', doc: props.auth.uid },
+        ...followeesConnection,
+        { collection: 'events' }
+    ]
 }
 
 export default compose(
     connect(mapStateToProps),
-    firestoreConnect((props) => {
-        // console.log(props.auth.uid);
-        return [{ collection: 'users', doc: props.auth.uid }]
-    })
+    firestoreConnect(firestoreConnectArgs)
 )(Recommender);
