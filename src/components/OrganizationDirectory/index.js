78473a835d4cd54bbee626670322f0ce6a5d46e7
Colton Potter <cpotter2@oberlin.edu>
2020-05-29 15:23:32 -0400

    wip: Retrieve information from orgs collection

--- src/components/OrganizationDirectory/index.js
+++ src/components/OrganizationDirectory/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
+import { firestoreConnect, useFirestore } from 'react-redux-firebase';
 import styled from 'styled-components';
 
 const Div = styled.div`
@@ -15,29 +15,70 @@ const Div = styled.div`
   }
 `;
 
+const DetailsDiv = styled.div`
+  /* display: ${(props) => (props.displayMe ? 'block' : 'none')}; */
+  background: green;
+`;
+
 const StyledSearchDiv = styled.div`
   background: coral;
   text-align: center;
 `;
 
-const handleOrgClick = (e) => {
-  e.preventDefault();
-  const oid = e.target.dataset.orgid;
-  console.log(`This org has id ${oid}`);
-};
-
 const OrganizationDirectory = (props) => {
+  const firestore = useFirestore();
+  const [orgsListDisplay, setOrgsListDisplay] = useState({});
+  const [orgsListContent, setOrgsListContent] = useState({});
+
+  const handleOrgClick = (e) => {
+    e.preventDefault();
+    const oid = e.target.dataset.orgid;
+    console.log(`You clicked org with id ${oid}`);
+    if (orgsListDisplay[oid] && orgsListDisplay[oid] === true) {
+      // Close the div
+      setOrgsListDisplay({ ...orgsListDisplay, [oid]: false });
+      return;
+    }
+    // Else, get current org information and open the div
+    firestore
+      .collection('organizations')
+      .doc(oid)
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          console.error(`Organization with id ${oid} does not exist`);
+          return doc;
+        }
+        const data = doc.data();
+        console.log(
+          `Contact this org at: ${data.contact && data.contact.email}`
+        );
+        setOrgsListContent({ ...orgsListContent, [oid]: data });
+        setOrgsListDisplay({ ...orgsListDisplay, [oid]: true });
+        console.log(orgsListDisplay);
+        console.log(orgsListContent);
+      })
+      .catch((err) => console.error(err));
+  };
+
   if (!props.organizationList) return null;
+
   const orgList = props.organizationList.map((org) => {
     const attrList = org.attributes.join(', ');
+    const oid = org.id;
     const liProps = {
-      'data-orgid': org.id,
-      key: org.id,
+      'data-orgid': oid,
+      key: oid,
       onClick: handleOrgClick,
     };
+
+    // TODO(CP): Fix onClick inheritance
     return (
       <li {...liProps}>
         {org.name} has attributes {attrList}.
+        <DetailsDiv displayMe={orgsListDisplay[oid]}>
+          {orgsListContent[oid] ? JSON.stringify(orgsListContent[oid]) : null}
+        </DetailsDiv>
       </li>
     );
   });
