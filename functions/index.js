e34354f4dfb8fbb37d87a37805565a613d479262
Minh Lam <mlam@oberlin.edu>
2020-06-02 18:24:12 +0700

    wip: Assumed rec from Redis and gotten events by relevance

--- functions/index.js
+++ functions/index.js
@@ -21,3 +21,36 @@ exports.refreshEvents = functions.pubsub
   });
 
 exports.rateEvent = rateEvent;
+
+async function updateRecommendations() {
+  const LIMIT = 10;
+  const RECOMMENDATION = [
+    'Conservatory of Music',
+    'Composition',
+    'Undergraduate Research'
+  ];
+  const attrScore = {};
+  const relevanceScore = {};
+  const eventIds = []
+  RECOMMENDATION.forEach((attr, i, arr) => {
+    attrScore[attr] = length(arr) - i    // 1st attr in length-3 array scores 3, 
+  })                                     // 2nd scores 2 and 3rd scores 1
+  const querySnapshot = await db.collection('events').get();
+  querySnapshot.forEach(documentSnapshot => {
+    const eventId = documentSnapshot.id;
+    eventIds.push(eventId);
+    // eventFilters: {departments: Array<{id: string, name: string}>, 
+    //                event_types: Array<{id: string, name: string}>}
+    const eventFilters = documentSnapshot.get('filters');
+    const attributes = eventFilters
+      .departments
+      .concat(eventFilters.event_types)
+      .map(attrObj => attrObj.name);
+    relevanceScore[eventId] = attributes.reduce(
+      (total, current) => total + (attrScore[current] || 0),
+      0
+    );
+  })
+  eventId.sort((id1, id2) => relevanceScore[id2] - relevanceScore[id1]);
+  // TODO(ML): Write top 10 to person's events.recommendation
+}
