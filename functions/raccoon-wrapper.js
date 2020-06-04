8106f71606c360c7257a5293cce199eb9f3c5fb9
Minh Lam <mlam@oberlin.edu>
2020-06-04 07:24:17 +0700

    style: Removed comments, lots of em

--- functions/raccoon-wrapper.js
+++ functions/raccoon-wrapper.js
@@ -12,7 +12,7 @@ const raccoon = require('raccoon');
 /**
  * Update the raccoon redis according to the type
  */
-module.exports.rate = async function(userId, itemId, type) {
+module.exports.rate = async function (userId, itemId, type) {
   switch (type) {
     case 'like':
       await raccoon.liked(userId, itemId);
@@ -32,41 +32,12 @@ module.exports.rate = async function(userId, itemId, type) {
   return;
 };
 
-// /**
-//  * Update the raccoon redis with a like
-//  * @param {string} userId the ID of the user who liked
-//  * @param {string} itemId the ID of the item that was liked
-//  */
-// module.exports.liked = async function(userId, itemId) {
-//   return;
-// };
-
-// /**
-//  * Remove a like from the raccoon redis
-//  * @param {string} userId the ID of the user who liked
-//  * @param {string} itemId the ID of the item that was liked
-//  */
-// module.exports.unliked = async function(userId, itemId) {};
-
-// /**
-//  * Update the raccoon redis with a like
-//  * @param {string} userId the ID of the user who disliked
-//  * @param {string} itemId the ID of the item that was disliked
-//  */
-// module.exports.disliked = async function(userId, itemId) {};
-
-// /**
-//  * Remove a dislike from the raccoon redis
-//  * @param {string} userId the ID of the user who liked
-//  * @param {string} itemId the ID of the item that was liked
-//  */
-// module.exports.undisliked = async function(userId, itemId) {};
 
 /**
  * For testing: check the userId who liked an event
  * @param {string} itemId the ID of the item that was liked
  */
-module.exports.likedBy = async function(itemId) {
+module.exports.likedBy = async function (itemId) {
   return await raccoon.likedBy(itemId);
 };
 
@@ -74,6 +45,6 @@ module.exports.likedBy = async function(itemId) {
  * For testing: check the userId who disliked an event
  * @param {string} itemId the ID of the item that was liked
  */
-module.exports.dislikedBy = async function(itemId) {
+module.exports.dislikedBy = async function (itemId) {
   return await raccoon.dislikedBy(itemId);
 };
