09b291241c24542c291bbba4132a7ff301c9756d
Colton Potter <cpotter2@oberlin.edu>
2020-06-04 21:54:02 -0400

    feat: Links and routing to event details

--- src/components/EventDetails/index.js
+++ src/components/EventDetails/index.js
@@ -1,42 +1,77 @@
 import React from 'react';
-import { Switch, Route, useParams, useRouteMatch } from 'react-router-dom';
+import {
+  Switch,
+  Route,
+  useParams,
+  useRouteMatch,
+  Link,
+} from 'react-router-dom';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+const StyledEventsDiv = styled.div`
+  ul {
+    display: block;
+    list-style-type: decimal;
+  }
+
+  a,
+  a:visited {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
 
 const EventDetails = (props) => {
   let match = useRouteMatch();
 
-  let children;
-  // console.log(props.events);
+  let allEvents;
   if (props.events) {
-    children = props.events.map((event) => {
-      return <li>{JSON.stringify(event)}</li>;
-    });
+    allEvents = [];
+    for (const event in props.events) {
+      allEvents.push(
+        <Link to={`${match.path}/${event}`}>
+          <li>
+            <span style={{ fontWeight: 'bold' }}>
+              {props.events[event].title}
+            </span>
+            <br />
+            id: {event}
+          </li>
+        </Link>
+      );
+    }
   } else {
-    children = null;
+    allEvents = 'Loading...';
   }
 
   return (
-    <div id="event-details-div">
+    <StyledEventsDiv id="event-details-div">
       <Switch>
         <Route path={`${match.path}/:eventId`}>
           <Event {...props} />
         </Route>
         <Route path={match.path}>
-          <p>Hello, world!</p>
-          <ul>{children}</ul>
+          <h1>All Events</h1>
+          <ul>{allEvents}</ul>
         </Route>
       </Switch>
-    </div>
+    </StyledEventsDiv>
   );
 };
 
 const Event = (props) => {
   let { eventId } = useParams();
-  // const event = props.events ? props.events[eventId] : null;
+  const event = props.events ? props.events[eventId] : null;
 
-  return <h1>Matched event id: {eventId}</h1>;
+  return (
+    <div>
+      <h1>{event ? event['title'] : 'Loading...'}</h1>
+      <p>{JSON.stringify(event)}</p>
+    </div>
+  );
 };
 
 export default connect(({ firestore }) => ({
-  events: firestore.ordered && firestore.ordered.events,
+  events: firestore.data && firestore.data.events,
 }))(EventDetails);
