d7155467a851c195b1e3c29b086f3588fed1da4d
Colton Potter <cpotter2@oberlin.edu>
2020-06-12 20:38:56 -0400

    fix: follow reducer null reference error

--- src/reducers/followReducer.js
+++ src/reducers/followReducer.js
@@ -3,10 +3,10 @@ import { FOLLOW_ERROR, FOLLOW_REQUEST_SENT, FOLLOWING } from "../actions/types";
 const followReducer = (state = {}, action) => {
   switch (action.type) {
     case FOLLOWING:
-      console.log(`Now following user ${action.followeeId}`);
+      console.log(`Now following user ${action.payload.followeeId}`);
       return {};
     case FOLLOW_REQUEST_SENT:
-      console.log(`Follow request sent to ${action.followeeId}`);
+      console.log(`Follow request sent to ${action.payload.followeeId}`);
       return {};
     case FOLLOW_ERROR:
       console.log("Error following user: ", action.error);
